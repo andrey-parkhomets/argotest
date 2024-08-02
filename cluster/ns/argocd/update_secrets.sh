84a267a2c2f4dff9f59f9cbf281109aa6e05da79
Andrey Parkhomets <andrey.parkhomets@gmail.com>
2024-08-02 18:38:34 +0200

    update

---
+++ cluster/ns/argocd/update_secrets.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+encryptedClientSecret=$(gopass show -o github.com/organizations/klf2000org/settings/applications/2568443 ClientSecret| \
+kubeseal --namespace argocd \
+ --controller-namespace sealed-secrets \
+ --controller-name sealed-secrets \
+ --name argocd-oidc-secret \
+ --raw --from-file /dev/stdin)
+yq -i e  '.oidc.oauth.encryptedClientSecret |= "'"$encryptedClientSecret"'"' values_my-argocd-secrets.yaml
+encryptedClientId=$(gopass show -o github.com/organizations/klf2000org/settings/applications/2568443 ClientId | \
+kubeseal --namespace argocd \
+ --controller-namespace sealed-secrets \
+ --controller-name sealed-secrets \
+ --name argocd-oidc-secret \
+ --raw --from-file /dev/stdin)
+yq -i e  '.oidc.oauth.encryptedClientId |= "'"$encryptedClientId"'"' values_my-argocd-secrets.yaml 
