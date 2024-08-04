9ca9f8bdac9391a7f0d97e11d49d2b26e2f9dcff
Andrey Parkhomets <andrey.parkhomets@gmail.com>
2024-08-04 16:19:25 +0200

    fix

--- cluster/ns/argocd/install.sh
+++ cluster/ns/argocd/install.sh
@@ -8,6 +8,10 @@ done
 kubectl -n sealed-secrets wait --for=condition=Available=true deployment/sealed-secrets
 bash -c ./update_secrets.sh
 sleep 20
-kubectl rollout restart deployment -n argocd
+kubectl rollout restart deployment/argocd-dex-server -n argocd
+kubectl rollout restart deployment/argocd-repo-server -n  argocd
+kubectl rollout restart deployment/argocd-server -n argocd
+kubectl rollout restart deployment/argocd-applicationset-controller -n argocd
 sleep 20
 kubectl -n argocd wait --for=condition=Available deployment/argocd-dex-server
+kubectl -n argocd wait --for=condition=Available deployment/argocd-server
