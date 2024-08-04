19b993d93c6064428fc4d15c108ee8ff72342f8e
Andrey Parkhomets <andrey.parkhomets@gmail.com>
2024-08-05 00:35:35 +0200

    fix

--- cluster/ns/argocd/install.sh
+++ cluster/ns/argocd/install.sh
@@ -2,17 +2,21 @@
 rm -rf helm-charts/argo-cd-*.tgz helm-charts/sealed-secrets-*.tgz
 exit_code=1
 until [[ $exit_code -eq 0 ]]; do 
-kubectl kustomize --enable-helm |kubectl apply -f -
+kustomize build --enable-helm . |kubectl apply --force=true -l app==sealedsecret -f -
 exit_code=$?
 done
 kubectl -n sealed-secrets wait --for=condition=Available=true deployment/sealed-secrets
 bash -c ./update_secrets.sh
-kubectl -n argocd wait --for=condition=Available deployment/argocd-dex-server
+until [[ $exit_code -eq 0 ]]; do 
+kustomize build --enable-helm . |kubectl apply --force=true -l app!=sealedsecret -f -
+exit_code=$?
+kubectl -n argocd wait --for=condition=Available deployment/argocd-applicationset-controller
 kubectl -n argocd wait --for=condition=Available deployment/argocd-server
 kubectl -n argocd wait --for=condition=Available deployment/argocd-repo-server
 kubectl -n argocd wait --for=condition=Available deployment/argocd-redis
+sleep 10
 argocd login --core
-argocd app sync argocd --kube-context argocd --force
+argocd app sync argocd --kube-context argocd --force --prune --replace
 sleep 10
 kubectl rollout restart deployment/sealed-secrets -n sealed-secrets
 kubectl rollout restart deployment/argocd-dex-server -n argocd
