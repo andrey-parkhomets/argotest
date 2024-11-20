87cb6e1331889f4cb006ebfe685748e48ff3a2bd
Andrey Parkhomets <andrey.parkhomets@gmail.com>
2024-11-20 18:09:36 +0100

    new struct3

--- cluster/ns/argocd/update_secrets.sh
+++ cluster/ns/argocd/update_secrets.sh
@@ -8,14 +8,14 @@ kubeseal --namespace argocd \
  --controller-name sealed-secrets \
  --name argocd-oidc-secret \
  --raw --from-file /dev/stdin)
-yq -i e  '.spec.encryptedData."oidc.auth0.clientSecret" |= "'"$encryptedClientSecret"'"' sealedsecret_argocd-oidc-secret.yaml
+yq -i e  '.spec.encryptedData.oidc\.auth0\.clientSecret |= "'"$encryptedClientSecret"'"' sealedsecret_argocd-oidc-secret.yaml
 encryptedClientId=$(gopass show -o github.com/organizations/klf2000org/settings/applications/2568443 ClientId | \
 kubeseal --namespace argocd \
  --controller-namespace sealed-secrets \
  --controller-name sealed-secrets \
  --name argocd-oidc-secret \
  --raw --from-file /dev/stdin)
-yq -i e  '.spec.encryptedData."oidc.auth0.clientId |= "'"$encryptedClientId"'"' sealedsecret_argocd-oidc-secret.yaml
+yq -i e  '.spec.encryptedData.oidc\.auth0\.clientId |= "'"$encryptedClientId"'"' sealedsecret_argocd-oidc-secret.yaml
 git -P diff
 git add SealedSecret_argocd-oidc-secret.yaml
 git commit -m 'update sealed secrets values'
